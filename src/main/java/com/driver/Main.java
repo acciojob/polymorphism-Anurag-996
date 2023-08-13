6fa5a3168b16f9d0f51d4a9cb67cf02b926543c4
Anurag-996 <anuragvishwakarma546@gmail.com>
2023-08-13 10:40:20 +0530

    Polymorphsim

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,50 @@
 package com.driver;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Product p = new Product();
+//        p.product(2,5);
+//        p.product(2,5,4);
+//        p.product(3.4,4.47);
+//    }
+//    static class Product{
+//        public int product(int x,int y){
+//            return x*y;
+//        }
+//        public int product(int x,int y,int z){
+//            return x*y*z;
+//        }
+//        public double product(double x,double y){
+//            return x*y;
+//        }
+//    }
+//
+//}
+class Main {
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
-public class Main {
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(2,5);
-        p.product(2,5,4);
-        p.product(3.4,4.47);
-    }
-    static class Product{
-        public int product(int x,int y){
-            return x*y;
-        }
-        public int product(int x,int y,int z){
-            return x*y*z;
-        }
-        public double product(double x,double y){
-            return x*y;
-        }
-    }
 
+        int result1 = p.product(2, 3);
+        System.out.println("Result 1: " + result1);
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Result 2: " + result2);
+
+        double result3 = p.product(2.5, 3.0);
+        System.out.println("Result 3: " + result3);
+    }
 }
